04d6647727b175021396138b52f935bc9c8f749b
cadulik <cadulrizzo@gmail.com>
2025-03-20 11:05:47 +0000

    creating players and enemys and add moovement for them

--- Teste/Code/entityFactory.py
+++ Teste/Code/entityFactory.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-from Code.Const import WIN_WIDHT
+# -*- coding: utf-8 -*
+import random
+
+from Code.Const import WIN_WIDHT, WIN_HEIGHT
 from Code.background import Background
+from Code.enemy import Enemy
+from Code.player import Player
 
 
 class EntityFactory:
@@ -15,4 +19,16 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}',(0,0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDHT, 0)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1',(10,WIN_HEIGHT/2 - 30))
+
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 + 30 ))
+
+            case 'Enemy1':
+                return Enemy('Enemy1',(WIN_WIDHT + 10 , random.randint(40, WIN_HEIGHT - 40)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDHT + 10, random.randint(40, WIN_HEIGHT -40)))
+
+
 
